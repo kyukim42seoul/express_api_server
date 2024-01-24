bd069477eb1acdf4d1dafec87b941e83c5a90b12
kyukim <kyukim@student.42seoul.kr>
2024-01-24 19:23:07 +0900

    [UPDATE] getFollowByUserId

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -1,17 +1,11 @@
 import db from "../database/db.js";
 
-/**
- * @param uploadTime
- * @param threadContent
- * @param userId
- */
-
 const createThread = (req, res) => {
 	const {uploadTime, threadContent, userId} = req.body;
 	const fields = `(upload_time, thread_content, user_id)`;
 	const fullQuery = `INSERT INTO thread ${fields} VALUES (?, ?, ?)`;
 
-	function convertToMySQLFormat(inputString) {
+	const convertToMySQLFormat = (inputString) => {
 		const date = new Date(inputString);
 	  
 		const pad = (num) => num.toString().padStart(2, '0');
@@ -26,7 +20,7 @@ const createThread = (req, res) => {
 		return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 	  };
 	
-	  const mysqlDateFormat = convertToMySQLFormat(uploadTime);
+	const mysqlDateFormat = convertToMySQLFormat(uploadTime);
 
 	const values = [mysqlDateFormat, threadContent, userId];
 	
@@ -54,29 +48,63 @@ const createThread = (req, res) => {
 	});
 };
 
+
+
 const getThread = (req, res) => {
 	const { userId } = req.query;
-	const selectQuery = "SELECT * FROM thread WHERE user_id = ? ORDER BY upload_time DESC LIMIT 10";
+	let userInfo = [{}];
+	let follow = [];
+	let idCollection = [];
 
+	// user_id 로 follow 가져오기
 	db.getConnection((error, connection) => {
+		const Query = "SELECT follow FROM users WHERE user_id = ?";
+		
+		if (error) {
+			connection.release();
+			return ;
+		}
+		try {
+			connection.query(Query, [userId], (error, result) => {
+				if (error) {
+					connection.release();
+					return ;
+				}
+				follow = result[0]["follow"];
+				idCollection = [...follow, userId];
+				connection.release();
+				return ;
+			});
+		} catch(error) {
+			connection.release();
+			return ;
+		}
+	});
+
+	// user_id 로 user_name, user_email 가져오기
+	// idCollection 으로 user_name, user_email 가져오기
+	db.getConnection((error, connection) => {
+		const Query = "SELECT user_name, user_email FROM users WHERE user_id = ?";
+		//const Query = `SELECT user_id, user_name, user_email FROM users WHERE user_id IN (?, ?, ?)`;
+		//let testUserInfo;
+
 		if (error) {
 			console.error(`ERR_getThread : `, error);
 			res.status(500).send("Fail get thread");
 			return ;
 		}
-		console.log(`getUserId : ${userId}`);
 		try {
-			console.log("start querying");
-			connection.query(selectQuery, [userId], (error, result) => {
+			connection.query(Query, [userId], (error, result) => {
+			//connection.query(Query, idCollection, (error, result) => {
 				if (error) {
 					console.error("ERR_getThread : ", error);
 					res.status(500).send("getThread failed");
 					return ;
 				}
-				console.log("done querying", result);
-				res.status(200).send(result);
+				userInfo = result;
+				//testUserInfo = result;
+				//console.log("testUserInfo", testUserInfo);
 				connection.release();
-				console.log("end connection");
 				return ;
 			});
 		} catch(error) {
@@ -84,9 +112,50 @@ const getThread = (req, res) => {
 			connection.release();
 			return ;
 		}
-		console.log('connection callback over');
 	});
+	// Promise 방식으로 전환할 때 사용할 부분
+	//console.log("userInfo", userInfo);
+
+	// userId 가 일치하는 thread 정보 가져오기
+	db.getConnection((error, connection) => {
+		const selectQuery = "SELECT * FROM thread WHERE user_id = ? ORDER BY upload_time DESC LIMIT 10"; // user_id 가 하나인 경우
+		//const selectQuery = `SELECT * FROM thread WHERE user_id IN (?, ?, ?) ORDER BY upload_time DESC LIMIT 10`;
+		let infoCollection = {};
+
+		if (error) {
+			console.error(`ERR_getThread : `, error);
+			res.status(500).send("Fail get thread");
+			return ;
+		}
+		try {
+			connection.query(selectQuery, [userId], (error, result) => {
+			//connection.query(selectQuery, idCollection, (error, result) => {
+				if (error) {
+					console.error("ERR_getThread : ", error);
+					res.status(500).send("getThread failed");
+					return ;
+				}
+				infoCollection = result.map((threadInfo) => {return {...userInfo[0], ...threadInfo}});
+
+				res.status(200).send(infoCollection);
+				connection.release();
+				return;
+			});
+		} catch(error) {
+			console.error("Query error : ", error);
+			connection.release();
+			return ;
+		}
+	});
+
+	// Promise 방식으로 전환할 때 사용할 부분
+	//console.log("threadInfo", threadInfo);
+	//const infoCollection = threadInfo.map((thread) => {return {...selectedUserInfo[0], ...thread}}) || {};
+
+	//res.status(200).send(infoCollection);
 };
+
+
 
 export default {
 	createThread: createThread,
