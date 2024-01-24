28d3670c644ce4e23778c95b961884a3c8396c4a
kyukim <kyukim@student.42seoul.kr>
2024-01-24 20:20:31 +0900

    [UPDATE] getMergedDataByUserId

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -52,41 +52,10 @@ const createThread = (req, res) => {
 
 const getThread = (req, res) => {
 	const { userId } = req.query;
-	let userInfo = [{}];
-	let follow = [];
-	let idCollection = [];
 
-	// user_id 로 follow 가져오기
+	// userId 의 followee 를 포함한 ids 가 일치하는 thread 정보 및 userName, userEmail 가져오기
 	db.getConnection((error, connection) => {
-		const Query = "SELECT follow FROM users WHERE user_id = ?";
-		
-		if (error) {
-			connection.release();
-			return ;
-		}
-		try {
-			connection.query(Query, [userId], (error, result) => {
-				if (error) {
-					connection.release();
-					return ;
-				}
-				follow = result[0]["follow"];
-				idCollection = [...follow, userId];
-				connection.release();
-				return ;
-			});
-		} catch(error) {
-			connection.release();
-			return ;
-		}
-	});
-
-	// user_id 로 user_name, user_email 가져오기
-	// idCollection 으로 user_name, user_email 가져오기
-	db.getConnection((error, connection) => {
-		const Query = "SELECT user_name, user_email FROM users WHERE user_id = ?";
-		//const Query = `SELECT user_id, user_name, user_email FROM users WHERE user_id IN (?, ?, ?)`;
-		//let testUserInfo;
+		const selectQuery = "SELECT t.*, u.user_name, u.user_email FROM thread t JOIN users u ON t.user_id = u.user_id WHERE t.user_id = ? OR t.user_id IN (SELECT followee_id FROM follows WHERE follower_id = ?) ORDER BY t.upload_time DESC;"
 
 		if (error) {
 			console.error(`ERR_getThread : `, error);
@@ -94,50 +63,13 @@ const getThread = (req, res) => {
 			return ;
 		}
 		try {
-			connection.query(Query, [userId], (error, result) => {
-			//connection.query(Query, idCollection, (error, result) => {
+			connection.query(selectQuery, [userId, userId], (error, result) => {
 				if (error) {
 					console.error("ERR_getThread : ", error);
 					res.status(500).send("getThread failed");
 					return ;
 				}
-				userInfo = result;
-				//testUserInfo = result;
-				//console.log("testUserInfo", testUserInfo);
-				connection.release();
-				return ;
-			});
-		} catch(error) {
-			console.error("Query error : ", error);
-			connection.release();
-			return ;
-		}
-	});
-	// Promise 방식으로 전환할 때 사용할 부분
-	//console.log("userInfo", userInfo);
-
-	// userId 가 일치하는 thread 정보 가져오기
-	db.getConnection((error, connection) => {
-		const selectQuery = "SELECT * FROM thread WHERE user_id = ? ORDER BY upload_time DESC LIMIT 10"; // user_id 가 하나인 경우
-		//const selectQuery = `SELECT * FROM thread WHERE user_id IN (?, ?, ?) ORDER BY upload_time DESC LIMIT 10`;
-		let infoCollection = {};
-
-		if (error) {
-			console.error(`ERR_getThread : `, error);
-			res.status(500).send("Fail get thread");
-			return ;
-		}
-		try {
-			connection.query(selectQuery, [userId], (error, result) => {
-			//connection.query(selectQuery, idCollection, (error, result) => {
-				if (error) {
-					console.error("ERR_getThread : ", error);
-					res.status(500).send("getThread failed");
-					return ;
-				}
-				infoCollection = result.map((threadInfo) => {return {...userInfo[0], ...threadInfo}});
-
-				res.status(200).send(infoCollection);
+				res.status(200).send(result);
 				connection.release();
 				return;
 			});
@@ -147,12 +79,6 @@ const getThread = (req, res) => {
 			return ;
 		}
 	});
-
-	// Promise 방식으로 전환할 때 사용할 부분
-	//console.log("threadInfo", threadInfo);
-	//const infoCollection = threadInfo.map((thread) => {return {...selectedUserInfo[0], ...thread}}) || {};
-
-	//res.status(200).send(infoCollection);
 };
 
 
